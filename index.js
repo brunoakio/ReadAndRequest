114068f4c96e8622f5aa4858310c374de268fd28
Bruno Akio Branco <brunoakiob@gmail.com>
2024-05-14 18:45:01 -0300

    teste

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-import express from "express";
+import express from 'express';
+import fs from 'fs';
+import axios from 'axios';
+import csv from 'csv-parser';
 
 const app = express();
 
 app.use(express.json());
 
-const fs = require('fs');
-const axios = require('axios');
-const csv = require('csv-parser');
-
 // Função para ler o arquivo CSV
-function lerArquivoCSV(caminhoArquivo) {
-    const dados = [];
-    
+function readCsv(filePath) {
     return new Promise((resolve, reject) => {
-        fs.createReadStream(caminhoArquivo)
+        const dados = [];
+        fs.createReadStream(filePath)
             .pipe(csv())
             .on('data', (row) => {
+                //console.log(row);
                 dados.push(row);
             })
             .on('end', () => {
@@ -28,28 +27,27 @@ function lerArquivoCSV(caminhoArquivo) {
 }
 
 // Função para enviar os dados via API
-async function enviarDadosViaAPI(dados, urlDaAPI) {
-    try {
-        const resposta = await axios.post(urlDaAPI, dados);
-        console.log('Dados enviados com sucesso:', resposta.data);
-    } catch (erro) {
-        console.error('Erro ao enviar os dados via API:', erro);
-    }
-}
+// async function sendApi(dados, urlApi) {
+//     try {
+//         const resposta = await axios.post(urlApi, dados);
+//         console.log('Dados enviados com sucesso:', resposta.data);
+//     } catch (erro) {
+//         console.error('Erro ao enviar os dados via API:', erro);
+//     }
+// }
 
 // Caminho do arquivo CSV e URL da API
-const caminhoArquivoCSV = 'dados.csv';
-const urlDaAPI = 'http://sua-api.com/endpoint';
+const filePath = 'teste.csv';
+const urlApi = 'https://message-template-api.omni.chat/v1/templates/LqXKSR78Kb';
 
-// Ler o arquivo CSV e enviar os dados via API ttt
-lerArquivoCSV(caminhoArquivoCSV)
+// Ler o arquivo CSV e enviar os dados via API
+readCsv(filePath)
     .then((dados) => {
-        enviarDadosViaAPI(dados, urlDaAPI);
+        console.log((dados[0].coluna3));
+        //sendApi(dados, urlApi);
     })
     .catch((erro) => {
         console.error('Erro ao ler o arquivo CSV:', erro);
     });
-
-
 
 app.listen(3333);
